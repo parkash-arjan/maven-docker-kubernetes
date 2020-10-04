f2587ff4bf12490db9f0845bb737b142f32b4a8a
Parkash Arjan <Arjan.Parkash.Git@gmail.com>
2020-10-04 14:23:56 -0400

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+W
+'src/main/java/com/euler/HelloMaven.java,7\e\7eeddf6456d6fd11446b5e31becda1f5935ea70b
---
+++ src/main/java/com/euler/HelloMaven.java
@@ -0,0 +1,9 @@
+package com.euler;
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven");
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
